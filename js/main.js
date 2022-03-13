4ec08f5eff0e57ebe697828e8efaf35b57e93e6e
mrp4sten <mauricio.pasten.martinez@gmail.com>
2022-03-12 20:22:06 -0600

    sending orders

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+const API_URI = "https://serverless-aq7pou9ax-mrp4sten.vercel.app";
+
 const stringToHTML = (s) => {
   const parser = new DOMParser();
   const doc = parser.parseFromString(s, "text/html");
@@ -15,16 +17,40 @@ const renderItem = (item) => {
     );
     element.classList.add("selected");
 
-    const mealsIdInput = document.getElementById('meals-id');
+    const mealsIdInput = document.getElementById("meals-id");
     mealsIdInput.value = item._id;
-
   });
 
   return element;
 };
 
 window.onload = () => {
-  fetch("https://serverless-aq7pou9ax-mrp4sten.vercel.app/api/meals")
+  const orderForm = document.getElementById("order");
+  orderForm.onsubmit = (e) => {
+    e.preventDefault();
+    const mealId = document.getElementById("meals-id");
+    const mealIDValue = mealId.value;
+
+    if (!mealIDValue) {
+      alert("You should select a meal");
+      return;
+    }
+
+    const order = {
+      meal_id: mealIDValue,
+      user_id: "user",
+    };
+
+    fetch(API_URI + "/api/orders", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(order),
+    }).then((x) => console.log(x));
+  };
+
+  fetch(API_URI + "/api/meals")
     .then((res) => res.json())
     .then((data) => {
       const mealsList = document.getElementById("meals-list");
