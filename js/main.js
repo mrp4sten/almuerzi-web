665bff7819b3caa7728bdb509d5be538b69a0ea6
mrp4sten <mauricio.pasten.martinez@gmail.com>
2022-03-12 18:34:41 -0600

    showin meals list

---
+++ js/main.js
@@ -0,0 +1,11 @@
+window.onload = () => {
+    fetch('https://serverless-aq7pou9ax-mrp4sten.vercel.app/api/meals')
+        .then(res => res.json())
+        .then(data => {
+            const mealsList = document.getElementById('meals-list');
+            const template = data.map(x => '<li>' + x.name + '</li>').join('');
+
+            mealsList.innerHTML = template;
+            console.log(data);
+        });
+}
