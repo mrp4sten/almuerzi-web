b228d8e78414617c94fc807f400c7b6a5124e4fe
mrp4sten <mauricio.pasten.martinez@gmail.com>
2022-03-12 19:53:53 -0600

    controling meal list

--- js/main.js
+++ js/main.js
@@ -1,11 +1,36 @@
+const stringToHTML = (s) => {
+  const parser = new DOMParser();
+  const doc = parser.parseFromString(s, "text/html");
+
+  return doc.body.firstChild;
+};
+
+const renderItem = (item) => {
+  const element = stringToHTML(`<li id="${item._id}">${item.name}</li>`);
+
+  element.addEventListener("click", () => {
+    const mealsList = document.getElementById("meals-list");
+    Array.from(mealsList.children).forEach((x) =>
+      x.classList.remove("selected"),
+    );
+    element.classList.add("selected");
+  });
+
+  return element;
+};
+
 window.onload = () => {
-    fetch('https://serverless-aq7pou9ax-mrp4sten.vercel.app/api/meals')
-        .then(res => res.json())
-        .then(data => {
-            const mealsList = document.getElementById('meals-list');
-            const btnSubmit = document.getElementById('btnSubmit');
-            const template = data.map(x => '<li>' + x.name + '</li>').join('');
-            mealsList.innerHTML = template;
-            btnSubmit.removeAttribute('disabled'); 
-        });
-}
+  fetch("https://serverless-aq7pou9ax-mrp4sten.vercel.app/api/meals")
+    .then((res) => res.json())
+    .then((data) => {
+      const mealsList = document.getElementById("meals-list");
+      const btnSubmit = document.getElementById("btnSubmit");
+
+      const listItems = data.map(renderItem);
+
+      mealsList.removeChild(mealsList.firstElementChild);
+      listItems.forEach((element) => mealsList.appendChild(element));
+
+      btnSubmit.removeAttribute("disabled");
+    });
+};
