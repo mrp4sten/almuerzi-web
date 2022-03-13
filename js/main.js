6803a61ddee4e30c0bc7833c7b8912fd9a4cc28e
mrp4sten <mauricio.pasten.martinez@gmail.com>
2022-03-12 18:40:41 -0600

    disabling/enabling button submit

--- js/main.js
+++ js/main.js
@@ -3,9 +3,9 @@ window.onload = () => {
         .then(res => res.json())
         .then(data => {
             const mealsList = document.getElementById('meals-list');
+            const btnSubmit = document.getElementById('btnSubmit');
             const template = data.map(x => '<li>' + x.name + '</li>').join('');
-
             mealsList.innerHTML = template;
-            console.log(data);
+            btnSubmit.removeAttribute('disabled'); 
         });
 }
